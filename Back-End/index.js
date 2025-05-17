a9d0db64ca0512b18d05f9ded2104ee4d79589ba
Mukesh Kumar <111741975+MukeshKumarCoder@users.noreply.github.com>
2025-05-18 02:01:14 +0530

    Update index.js

--- Back-End/index.js
+++ Back-End/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8000;
 // middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://booklibrarymanagment.netlify.app/",
     credentials: true,
   })
 );
