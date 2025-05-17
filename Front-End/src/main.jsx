a66501e0d8db59152d787b1f8e4db953dbfc0c5a
Mukesh Kumar <111741975+MukeshKumarCoder@users.noreply.github.com>
2025-05-18 01:51:43 +0530

    Update main.jsx

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -1,4 +1,4 @@
-
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -9,10 +9,12 @@ import { Provider } from "react-redux";
 import store from "./Redux/store.js";
 
 createRoot(document.getElementById("root")).render(
+   <StrictMode>
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
     <ToastContainer />
   </BrowserRouter>
+  </StrictMode>
 );
